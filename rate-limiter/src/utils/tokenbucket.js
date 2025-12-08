b5fc3fdf93c2a86e830c38b899c181a352b49059
utkarsh <utkarshsingh132003@gmail.com>
2025-12-08 20:22:15 +0530

    opyimized

--- rate-limiter/src/utils/tokenbucket.js
+++ rate-limiter/src/utils/tokenbucket.js
@@ -1,26 +1,29 @@
-export function createBucket(capacity, refillPerSecond){
-    return {
-        capacity,
-        tokens: capacity,
-        refillPerSecond,
-        lastRefill: Date.now()
-    }
+export function createBucket(capacity, refillPerSecond) {
+  return {
+    capacity,
+    tokens: capacity,
+    refillPerSecond,
+    lastRefill: Date.now(),
+  };
 }
 
-export function Refill(bucket){
-    const intervalInSec = (Date.now() - bucket.lastRefill) / 1000;
+export function Refill(bucket) {
+  const intervalInSec = (Date.now() - bucket.lastRefill) / 1000;
+  if (intervalInSec > 0) {
     const addToken = intervalInSec * bucket.refillPerSecond;
     bucket.tokens = Math.min(addToken + bucket.tokens, bucket.capacity);
+    bucket.lastRefill = Date.now();
+  }
 }
 
-export function tryAndRemove(bucket){
-    Refill(bucket);
+export function tryAndRemove(bucket) {
+  Refill(bucket);
 
-    const tokenLeft = bucket.tokens;
-    if(tokenLeft >= 1){
-        bucket.tokens = bucket.tokens - 1;
-        return true;
-    }
+  const tokenLeft = bucket.tokens;
+  if (tokenLeft >= 1) {
+    bucket.tokens = bucket.tokens - 1;
+    return true;
+  }
 
-    return false;
+  return false;
 }
