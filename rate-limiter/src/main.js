9e7f8a735dfe4b3e9285ee93b7eb1153b5328791
utkarsh <utkarshsingh132003@gmail.com>
2025-12-09 21:51:05 +0530

    global bucket

--- rate-limiter/src/main.js
+++ rate-limiter/src/main.js
@@ -3,24 +3,20 @@ import { createBucket, tryAndRemove } from "./utils/tokenbucket.js";
 
 const app = express();
 
-const buckets = {};
+let globalBucket = createBucket(10, 0.1);
+function RateLimiter(req, res, next){
+  console.log("Global Bucket status is: ", globalBucket)
 
-export function RateLimiter(req, res, next){
-  const ip = req.ip;
-
-  if(!buckets[ip]){
-   buckets[ip] = createBucket(10, 0.01);
+  if(!tryAndRemove(globalBucket)){
+    return res.status(429).json({message: "Request Limit Exceeded", status: false})
   }
 
-  const userBucket = buckets[ip];
-  console.log("User bucket status is: ", userBucket)
-  if(!tryAndRemove(userBucket)){
-    return res.status(429).json({message: "Pajji limited exceeded, Please try again after some time"});
-  }
-  next();
+  return next();
+
 }
 
-app.use(RateLimiter)
+app.use(RateLimiter);
+
 
 app.get("/health", (req, res) => {
   console.log(req.ip);
--- rate-limiter/src/utils/tokenbucket.js
+++ rate-limiter/src/utils/tokenbucket.js
@@ -1,26 +1,26 @@
-export function createBucket(capacity, refillPerSecond) {
+export function createBucket(capacity, refillPerSecond){
   return {
     capacity,
     tokens: capacity,
     refillPerSecond,
-    lastRefill: Date.now(),
-  };
-}
-
-export function Refill(bucket) {
-  const intervalInSec = (Date.now() - bucket.lastRefill) / 1000;
-  if (intervalInSec > 0) {
-    const addToken = intervalInSec * bucket.refillPerSecond;
-    bucket.tokens = Math.min(addToken + bucket.tokens, bucket.capacity);
-    bucket.lastRefill = Date.now();
+    lastRefill: Date.now()
   }
 }
 
-export function tryAndRemove(bucket) {
-  Refill(bucket);
+export function refillBucket(bucket){
+  const now = Date.now();
+  const intvervalInSec = (now - bucket.lastRefill) / 1000;
+  if(intvervalInSec > 0){
+    const addTokens = intvervalInSec * bucket.refillPerSecond;
+    bucket.tokens = Math.min(addTokens + bucket.tokens, bucket.capacity)
+    bucket.lastRefill = now;
+  }
+}
 
-  const tokenLeft = bucket.tokens;
-  if (tokenLeft >= 1) {
+export function tryAndRemove(bucket){
+  refillBucket(bucket);
+
+  if(bucket.tokens >= 1){
     bucket.tokens = bucket.tokens - 1;
     return true;
   }
