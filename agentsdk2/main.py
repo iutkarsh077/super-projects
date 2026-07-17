7881b3a6d9de0332352efc807d5637dd388fee4a
iutkarsh077 <utkarshsingh132003@gmail.com>
2026-07-17 06:28:25 +0530

    tool calling

--- agentsdk2/main.py
+++ agentsdk2/main.py
@@ -1,19 +1,27 @@
 from fastapi import FastAPI, Response, HTTPException
-from agents import Agent, Runner
+from agents import Agent, Runner, function_tool
 from dotenv import load_dotenv
+import random
 
 load_dotenv()
 app = FastAPI()
 
-agent = Agent(name="AI Assistant", instructions="You are a helpful AI assistant, that answer user query by using tools")
+
+@function_tool
+def get_weather(city: str):
+    """Tells the weather of any city in the world"""
+    return f"The weather of {city} is {random.randint(1, 45)} degree celcius"
+
+agent = Agent(name="AI Assistant", instructions="You are a weather assistant. Whenever the user asks anything about weather temperature, climate, rain, forecast, or conditions of any city, you MUST use the get_weather tool. Never answer weather questions from your own knowledge.", model="gpt-5-nano", tools=[get_weather])
 
 @app.get("/ask-question")
-def AskQuestions(question: str, res: Response):
+async def AskQuestions(question: str, res: Response):
     try:
-        result = Runner.run_sync(agent, question)
+        result = await Runner.run(agent, question)
         if(not result.final_output):
             raise HTTPException(status_code=500, detail="Failed to get LLM response")
         res.status_code = 201
+        print(result)
         return { "message": "Got data successfully", "data": result.final_output }
     except Exception as e:
         print(e)
