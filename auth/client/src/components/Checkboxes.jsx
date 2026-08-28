ae4df37f9f8e4f1e619455a8aae03fafb6913768
iutkarsh077 <utkarshsingh132003@gmail.com>
2026-08-29 00:19:41 +0530

    multiple checboxes handle

---
+++ auth/client/src/components/Checkboxes.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react"
+
+const Checkboxes = () => {
+    const [checkboxes, setCheckboxes] = useState([
+        { id: 1, name: "First", checked: false },
+        { id: 2, name: "Second", checked: false },
+        { id: 3, name: "Third", checked: false },
+        { id: 4, name: "Fourth", checked: false },
+
+    ]);
+
+    const [isSelectedAll, setIsSelectedAll] = useState(false);
+
+    useEffect(()=>{
+        checkboxes.forEach((item)=> {
+            if(item.checked === false){
+                setIsSelectedAll(false);
+            }
+        })
+    }, [checkboxes])
+
+    const handleSelectAll = (isSelectedAll) => {
+        if (isSelectedAll) {
+            setCheckboxes((prev) => prev.map((item) => ({ ...item, checked: true })))
+        }
+
+        setIsSelectedAll(isSelectedAll);
+    }
+
+
+    const individualSelect = (id) => {
+        setCheckboxes((prev) => prev.map((item) => {
+            if (id === item.id) {
+                return { ...item, checked: !item.checked };
+            }
+
+            return item;
+        }))
+    }
+    return (
+        <>
+            <div className="ml-5 mt-5">
+                <div>
+                    <input type="checkbox" checked={isSelectedAll} disabled={isSelectedAll} onClick={() => handleSelectAll(!isSelectedAll)} />
+                    <span>Select All</span>
+                </div>
+                {checkboxes.map((item) => (
+                    <div className="flex items-center gap-3" key={item.id}>
+                        <input type="checkbox" checked={item.checked} onClick={() => individualSelect(item.id)} />
+                        <span>{item.name}</span>
+
+                    </div>
+                ))}
+            </div>
+        </>
+    )
+}
+
+export default Checkboxes
--- auth/client/src/routing/MyRoutes.jsx
+++ auth/client/src/routing/MyRoutes.jsx
@@ -3,12 +3,14 @@ import Signup from "../components/Signup";
 import Login from "../components/Login";
 import Dashboard from "../components/Dashboard";
 import ProtectedRoute from "./ProtectedRoute";
+import Checkboxes from "../components/Checkboxes";
 
 const MyRoutes = () => {
     return (
         <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
+            <Route path="/checkbox" element={<Checkboxes />} />
             <Route element={<ProtectedRoute />}>
                 <Route path="/" element={<Dashboard />} />
             </Route>
